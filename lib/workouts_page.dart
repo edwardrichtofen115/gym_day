afe121d17dc1f41e1136757712f5ff79bc94b02f
swapnil <swapnil@swapnils-MacBook-Pro.local>
2021-05-05 22:56:41 +0530

    Updated the delete functionality

--- lib/workouts_page.dart
+++ lib/workouts_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gym_day/checkin_form.dart';
 
 final _firestore = FirebaseFirestore.instance;
 final _auth = FirebaseAuth.instance;
@@ -19,61 +20,20 @@ class _WorkoutsPageState extends State<WorkoutsPage> {
 
   @override
   Widget build(BuildContext context) {
-    Future<List<CustomWorkoutWidget>> workouts = getWorkouts();
+
 
     return Scaffold(
-      body: FutureBuilder(
-        future: workouts,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _firestore.collection('workouts').orderBy('dateTime').snapshots(),
         builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data.length > 0) {
-            // ignore: missing_return
-            return ListView(children: [
-              Column(
-                children: snapshot.data,
-              ),
-            ]);
-            // ignore: missing_return
-          } else if (snapshot.data == null) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FaIcon(
-                  FontAwesomeIcons.cloudDownloadAlt,
-                  size: 70.0,
-                ),
-                SizedBox(
-                  height: 50.0,
-                ),
-                Center(
-                  child: Text(
-                    'Data being fetched. Please wait',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ],
-            );
-          } else if (snapshot.data.length == 0) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FaIcon(
-                  FontAwesomeIcons.sadCry,
-                  size: 70.0,
-                ),
-                SizedBox(
-                  height: 50.0,
-                ),
-                Center(
-                  child: Text(
-                    'No workouts exist yet.\n\nCheck in to see your workouts',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ],
+          if (snapshot.hasData) {
+            final workouts = snapshot.data.docs;
+            for(var workout in workouts){
+              print(workout.data()['workout']);
+            }
+            List<CustomWorkoutWidget> finalWorkouts = getWorkouts(workouts);
+            return ListView(
+              children: finalWorkouts,
             );
           } else
             return Text('Error');
@@ -136,33 +96,20 @@ class _CustomWorkoutWidgetState extends State<CustomWorkoutWidget> {
   }
 }
 
-Future<List<CustomWorkoutWidget>> getWorkouts() async {
+List<CustomWorkoutWidget> getWorkouts(List<QueryDocumentSnapshot> workouts)  {
 
-  List<CustomWorkoutWidget> workouts = [];
-  await _firestore
-      .collection('workouts')
-      .orderBy('dateTime', descending: true)
-      .get()
-      .then((value) {
-    value.docs.forEach((element) {
-      if (element.data()['user'] == _auth.currentUser.email) {
-        final temp = CustomWorkoutWidget(
-            workoutName: element.data()['workout'],
-            workoutLocation: element.data()['location'],
-            workoutDateTime: element.data()['dateTime'],
-            workoutId: element.data()['uid'],
+  List<CustomWorkoutWidget> finalWorkouts = [];
+  for(var workout in workouts){
+    if(workout.data()['user'] == _auth.currentUser.email){
+      var temp = CustomWorkoutWidget(
+        workoutId: workout.data()['uid'],
+        workoutName: workout.data()['workout'],
+        workoutDateTime: workout.data()['dateTime'],
+        workoutLocation: workout.data()['location'],
+      );
 
-        );
-
-
-        workouts.add(temp);
-      }
-    });
-  });
-
-  // if(workouts.isEmpty)
-  //   return [Text('No data exists for you. Please check in to see your data')];
-  // else
-
-  return workouts;
+      finalWorkouts.add(temp);
+    }
+  }
+  return finalWorkouts;
 }
